e2c8a1672a9909e5fcaecef35723d2d1236aa7b4
gaowenliang <gaowenliang100@gmail.com>
2019-06-06 09:34:53 +0800

    add apriltag_frontend

---
+++ include/camera_model/apriltag/TagDetector.h
@@ -0,0 +1,29 @@
+#ifndef TAGDETECTOR_H
+#define TAGDETECTOR_H
+
+#include <vector>
+
+#include "opencv2/opencv.hpp"
+
+#include "TagDetection.h"
+#include "TagFamily.h"
+#include "FloatImage.h"
+
+namespace AprilTags {
+
+class TagDetector {
+public:
+	
+	const TagFamily thisTagFamily;
+
+	//! Constructor
+  // note: TagFamily is instantiated here from TagCodes
+	TagDetector(const TagCodes& tagCodes, const size_t blackBorder=2) : thisTagFamily(tagCodes, blackBorder) {}
+	
+	std::vector<TagDetection> extractTags(const cv::Mat& image);
+	
+};
+
+} // namespace
+
+#endif
---
+++ src/apriltag/MathUtil.cc
@@ -0,0 +1,11 @@
+#include "camera_model/apriltag/MathUtil.h"
+
+namespace AprilTags{
+
+// Output operator for std::pair<float,float>, useful for debugging
+std::ostream& operator<<(std::ostream &os, const std::pair<float,float> &pt) {
+  os << pt.first << "," << pt.second;
+  return os;
+}
+
+} // namespace
